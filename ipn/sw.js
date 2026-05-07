dc3880ad2f1a382cedfa88c2de5be6bbd730ac1e
Burkotun <burkotun.uno@gmail.com>
2026-05-07 21:21:48 +0300

    Create sw.js

---
+++ ipn/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'uno-ipn-v1';
+const ASSETS = [
+  'index.html',
+  '../simple-style.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
