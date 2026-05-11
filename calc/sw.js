79ec6c7ed12f8d1f79324e0bed15091dddf46f6e
Burkotun <burkotun.uno@gmail.com>
2026-05-11 16:33:58 +0300

    Create sw.js

---
+++ calc/sw.js
@@ -0,0 +1,20 @@
+// Назва кешу для версійності
+const CACHE_NAME = 'calc-v1';
+const ASSETS = [
+  'index.html',
+  '../simple-style.css'
+];
+
+// Встановлення та кешування
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Робота в офлайн-режимі
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
