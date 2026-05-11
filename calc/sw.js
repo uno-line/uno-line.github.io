777730f94c7d627ad54caa1b84f69f4e971f908a
Burkotun <burkotun.uno@gmail.com>
2026-05-11 18:28:04 +0300

    Update sw.js

--- calc/sw.js
+++ calc/sw.js
@@ -18,3 +18,37 @@ self.addEventListener('fetch', (e) => {
     caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
+
+self.addEventListener('install', (event) => {
+    // Змушує новий SW активуватися відразу
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    // Очищення старого кешу
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Негайно беремо контроль над сторінкою
+    );
+});
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').then((reg) => {
+        reg.addEventListener('updatefound', () => {
+            const newWorker = reg.installing;
+            newWorker.addEventListener('statechange', () => {
+                if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
+                    // Коли новий SW встановлено, просто перезавантажуємо сторінку
+                    window.location.reload();
+                }
+            });
+        });
+    });
+}
